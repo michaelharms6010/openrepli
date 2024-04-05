a60b6faf49b06f10e45b14249fda3ee3b073518b
Michael Harms <michael.harms6010@gmail.com>
2024-04-05 12:18:31 -0500

    allow cross-origin requests for api

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ function cleanGPTReplyText(str) {
 
 function showLoadingSpinner() {
   const loadingSVG = document.createElement("div");
-  loadingSVG.id = "oneclickreply-spinner";
+  loadingSVG.id = "OpenRepli-spinner";
   loadingSVG.className = "loading-spinner";
   loadingSVG.style.marginRight = "10px";
   loadingSVG.innerHTML = `
@@ -34,15 +34,15 @@ function showLoadingSpinner() {
   const targetElement = document.querySelector('div[data-testid="tweetButton"');
   targetElement.parentNode.insertBefore(loadingSVG, targetElement);
 
-  const replyButton = document.querySelector("#oneclickreply-button");
+  const replyButton = document.querySelector("#OpenRepli-button");
   replyButton.style.visibility = "hidden";
 }
 
 function hideLoadingSpinner() {
-  const element = document.querySelector("#oneclickreply-spinner");
+  const element = document.querySelector("#OpenRepli-spinner");
   element.remove();
 
-  const replyButton = document.querySelector("#oneclickreply-button");
+  const replyButton = document.querySelector("#OpenRepli-button");
   replyButton.style.visibility = "visible";
 }
 
@@ -92,7 +92,7 @@ var genericErrMsg =
 
 function showModal(error) {
   const modal = document.createElement("div");
-  modal.setAttribute("id", "oneclickreply-modal");
+  modal.setAttribute("id", "OpenRepli-modal");
   modal.style.cssText = `
       display: block;
       position: fixed;
@@ -110,7 +110,7 @@ function showModal(error) {
   `;
 
   const header = document.createElement("div");
-  header.innerHTML = "oneclickreply";
+  header.innerHTML = "OpenRepli";
   header.style.cssText = `
       letter-spacing: 0.025em;
       text-align: center;
@@ -146,7 +146,7 @@ function showModal(error) {
       width: 100%;
   `;
   button.addEventListener("click", function () {
-    document.getElementById("oneclickreply-modal").remove();
+    document.getElementById("OpenRepli-modal").remove();
   });
 
   modal.appendChild(header);
@@ -254,7 +254,7 @@ async function injectTwitterButton() {
   }
 
   const button = document.createElement("button");
-  button.id = "oneclickreply-button";
+  button.id = "OpenRepli-button";
   button.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="artdeco-button__icon" height="20" width="20">
       <path fill-rule="evenodd" d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z" clip-rule="evenodd" />
@@ -299,7 +299,7 @@ const findClosestPostLinkedIn = (el) => {
 };
 
 function findClosestReplyButton(el) {
-  const target = el.querySelector("#oneclickreply-button");
+  const target = el.querySelector("#OpenRepli-button");
   if (target) {
     return target;
   }
@@ -404,7 +404,7 @@ async function injectLinkedInButton() {
 
     if (parentBox.children.length < 3) {
       const replyButton = document.createElement("div");
-      replyButton.id = "oneclickreply-button";
+      replyButton.id = "OpenRepli-button";
       replyButton.className =
         "comments-comment-box__detour-icons artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view";
       replyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="artdeco-button__icon" height="24" width="24">
